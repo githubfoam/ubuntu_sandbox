4b5e838252025a450a1697812a7df48357c029b6
githubfoam <lequestionis@gmail.com>
2021-06-12 12:19:05 +0300

    придёшься

--- scripts/deploy-nxlog.sh
+++ scripts/deploy-nxlog.sh
@@ -11,7 +11,7 @@ echo "##############################################"
 
 apt-get update -qq
 # apt-get install -qqy openjdk-7-jre-headless, nginx, pv, htop, socat, geoip-bin, git, make
-apt-get install -qqy openjdk-13-jre-headless, nginx, pv, htop, socat, geoip-bin, git, make
+# apt-get install -qqy openjdk-13-jre-headless, nginx, pv, htop, socat, geoip-bin, git, make
 
 
 echo "##############################################"
@@ -38,7 +38,9 @@ echo "##############################################"
 # Download NXLOG
 #https://nxlog.co/products/nxlog-community-edition/download
 wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb
-dpkg -i nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb
+dpkg -i nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb # method1
+# apt install ./nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb # method2
+
 # wget http://nxlog.org/system/files/products/files/1/nxlog-ce_2.8.1248_ubuntu_1404_amd64.deb
 # dpkg -i nxlog-ce_2.8.1248_ubuntu_1404_amd64.deb
 apt-get install -f
