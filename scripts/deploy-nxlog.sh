a952e7411ea8a51e4f0a0532c176d4ee10b7e957
githubfoam <lequestionis@gmail.com>
2021-06-12 12:30:12 +0300

    придёмся

--- scripts/deploy-nxlog.sh
+++ scripts/deploy-nxlog.sh
@@ -13,6 +13,21 @@ apt-get update -qq
 # apt-get install -qqy openjdk-7-jre-headless, nginx, pv, htop, socat, geoip-bin, git, make
 # apt-get install -qqy openjdk-13-jre-headless, nginx, pv, htop, socat, geoip-bin, git, make
 
+# Download and install Perl Modules
+wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/perl-modules-5.26_5.26.1-6_all.deb
+apt install ./perl-modules-5.26_5.26.1-6_all.deb
+
+# Download and install Openssl library
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+apt install ./libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+
+# Download and install GNU dbm library
+wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdbm/libgdbm5_1.14.1-6_amd64.deb
+apt install ./libgdbm5_1.14.1-6_amd64.deb
+
+# Download and install Perl libraries
+wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/libperl5.26_5.26.1-6_amd64.deb
+apt install ./libperl5.26_5.26.1-6_amd64.deb
 
 echo "##############################################"
 echo "# Configure UFW Firewall                     #"
@@ -38,12 +53,13 @@ echo "##############################################"
 # Download NXLOG
 #https://nxlog.co/products/nxlog-community-edition/download
 wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb
-# dpkg -i nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb # method1
-apt install ./nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb # method2
+dpkg -i nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb # method1
 
 # wget http://nxlog.org/system/files/products/files/1/nxlog-ce_2.8.1248_ubuntu_1404_amd64.deb
 # dpkg -i nxlog-ce_2.8.1248_ubuntu_1404_amd64.deb
 apt-get install -f
+
+service nxlog status
 
 # Remove default NXLOG configuration & replace
 # rm /etc/nxlog/nxlog.conf 
