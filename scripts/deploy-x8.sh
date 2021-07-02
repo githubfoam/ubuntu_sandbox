8cd87897416702da5a6edfcf401c48d9dcdb0fcc
githubfoam <lequestionis@gmail.com>
2021-07-02 23:45:28 +0300

    wc

--- scripts/deploy-x8.sh
+++ scripts/deploy-x8.sh
@@ -6,19 +6,21 @@ set -o xtrace
 # set -eox pipefail #safety for script
 
 echo "##############################################"
-echo "# Install x8                       #"
+echo "#             Install x8                     #"
 echo "##############################################"
 
 apt-get update -qq
-# rustc --version #rustc: command not found
-
-# curl https://sh.rustup.rs -sSf | sh -s -- -y
-# source $HOME/.cargo/env
-# rustc --version
-
 apt-get install -y cargo
 
+echo "##############################################"
+echo "#             Cargo Installed                #"
+echo "##############################################"
 # https://github.com/Sh1Yo/x8
 git clone https://github.com/Sh1Yo/x8
 cd x8
 cargo build --release
+echo "##############################################"
+echo "#             x8 installed from source       #"
+echo "##############################################"
+
+wc https://github.com/berzerk0/Probable-Wordlists/blob/master/Real-Passwords/Top12Thousand-probable-v2.txt
