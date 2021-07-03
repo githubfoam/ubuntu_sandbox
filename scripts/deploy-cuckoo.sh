99cef568a866e2a8932c7ba60750dc9e4e8be8f7
githubfoam <lequestionis@gmail.com>
2021-07-03 20:22:16 +0300

    add 18.04

--- scripts/deploy-cuckoo.sh
+++ scripts/deploy-cuckoo.sh
@@ -12,6 +12,14 @@ echo "##############################################"
 apt-get update -qq
 
 # https://cuckoo.sh/docs/installation/host/requirements.html
+# software packages from the apt repositories are required to get Cuckoo to install and run properly
+software packages from the apt repositories are required to get Cuckoo to install and run properly
 apt-get install -y python python-pip python-dev libffi-dev libssl-dev
 apt-get install - python-virtualenv python-setuptools
 apt-get install -y libjpeg-dev zlib1g-dev swig
+
+# In order to use the Django-based Web Interface, MongoDB is required
+apt-get install -y mongodb
+
+# In order to use PostgreSQL as database (our recommendation), PostgreSQL will have to be installed 
+apt-get install -y postgresql libpq-dev
