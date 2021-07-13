6a05c26855c90a0055556f247a3b57c8b9a4aa83
Your Name <you@example.com>
2021-07-13 16:35:45 +0300

    laves

--- scripts/deploy-x8.sh
+++ scripts/deploy-x8.sh
@@ -55,6 +55,25 @@ elif [[ $(lsb_release -rs) == "18.04" ]]; then
 
 elif [[ $(lsb_release -rs) == "16.04" ]]; then 
 
+       echo "Compatible version"
+       
+        apt-get install -y cargo
+        echo "##############################################"
+        echo "#             Cargo Installed                #"
+        echo "##############################################"
+        # https://github.com/Sh1Yo/x8
+        git clone https://github.com/Sh1Yo/x8
+        cd x8
+        cargo build --release
+        echo "##############################################"
+        echo "from source code (rust should be installed)  #"
+        echo "##############################################"
+
+        wget https://github.com/berzerk0/Probable-Wordlists/blob/master/Real-Passwords/Top12Thousand-probable-v2.txt
+        mv Top12Thousand-probable-v2.txt wordlist.txt
+        wc wordlist.txt 
+        ./x8 -u https://4rt.one/ -w wordlist.txt -c 7
+        
 else
        echo "Non-compatible version"
 fi
